85b919b229945bc08f6d6cef1e83494c59828c47
avp <nakulparmar15@gmail.com>
2019-03-09 15:41:52 +0530

    Updates

---
+++ precache-manifest.f54981c1e5fba4cebaaa8278365fe7d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bc86b84415e5fdabb92",
+    "url": "/phone-dir/static/css/main.19e434f1.chunk.css"
+  },
+  {
+    "revision": "6bc86b84415e5fdabb92",
+    "url": "/phone-dir/static/js/main.35ebc230.chunk.js"
+  },
+  {
+    "revision": "3aa54ad544210659027f",
+    "url": "/phone-dir/static/js/runtime~main.ef9c2d20.js"
+  },
+  {
+    "revision": "d06e19c4def104df2fc7",
+    "url": "/phone-dir/static/css/2.0775c5df.chunk.css"
+  },
+  {
+    "revision": "d06e19c4def104df2fc7",
+    "url": "/phone-dir/static/js/2.1afcc8a6.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/phone-dir/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/phone-dir/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/phone-dir/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/phone-dir/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/phone-dir/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7bc0844885cc7d9c6076eac093039cdc",
+    "url": "/phone-dir/index.html"
+  }
+];
